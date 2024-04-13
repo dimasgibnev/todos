c54d00fe29deec0927e6a7b40fa8e6a3f41d5f47
Dima Sgibnev <dimasgibnev@gmail.com>
2024-04-13 23:17:45 +0700

    add todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
-import styles from'./App.module.css';
+import { useEffect, useState } from 'react';
+import styles from './App.module.css';
 
 export const App = () => {
-	return <></>;
+	const [todoList, setTodoList] = useState([
+		{
+			userId: 1,
+			id: 201,
+			title: 'Выполнить домашнее задание - реализовать список дел',
+			completed: false,
+		},
+	]);
+
+	const [isComplited, setIsComplited] = useState(false);
+
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/todos')
+			.then((response) => response.json())
+			.then((loadedTodos) => setTodoList((prev) => [...prev, ...loadedTodos]));
+	}, []);
+
+	return (
+		<div className={styles.App}>
+			{todoList.map(({ id, title }) => (
+				<div className={styles.todo} key={id}>
+					<div className={styles['todo__content']}>
+						<strong>{title}</strong>
+						{isComplited && <p>Выполнено</p>}
+						<input
+							type="checkbox"
+							name="complited"
+							id={id}
+							onChange={() => setIsComplited(!isComplited)}
+						/>
+					</div>
+				</div>
+			))}
+		</div>
+	);
 };
