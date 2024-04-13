983093b5ca96987b38b6e9b6d9d1a31aa08a7ad2
Dima Sgibnev <dimasgibnev@gmail.com>
2024-04-14 02:13:45 +0700

    add checkbox for list

--- src/App.jsx
+++ src/App.jsx
@@ -11,26 +11,35 @@ export const App = () => {
 		},
 	]);
 
-	const [isComplited, setIsComplited] = useState(false);
-
 	useEffect(() => {
 		fetch('https://jsonplaceholder.typicode.com/todos')
 			.then((response) => response.json())
 			.then((loadedTodos) => setTodoList((prev) => [...prev, ...loadedTodos]));
 	}, []);
 
+	const handleCheck = (id) => {
+		const updatedList = todoList.map((todo) => {
+			if (todo.id === id) {
+				return { ...todo, completed: !todo.completed };
+			}
+			return todo;
+		});
+		setTodoList(updatedList);
+	};
+
 	return (
 		<div className={styles.App}>
-			{todoList.map(({ id, title }) => (
+			{todoList.map(({ id, title, completed }) => (
 				<div className={styles.todo} key={id}>
 					<div className={styles['todo__content']}>
 						<strong>{title}</strong>
-						{isComplited && <p>Выполнено</p>}
+						{completed && <p>Выполнено</p>}
 						<input
 							type="checkbox"
-							name="complited"
+							name="completed"
+							checked={completed}
 							id={id}
-							onChange={() => setIsComplited(!isComplited)}
+							onChange={() => handleCheck(id)}
 						/>
 					</div>
 				</div>
